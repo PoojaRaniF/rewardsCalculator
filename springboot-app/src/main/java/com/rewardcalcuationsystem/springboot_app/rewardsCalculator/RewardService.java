54838e853bd3a006d9029479bffef4c99281cadd
poojrani <pooja.rani@capgemini.com>
2025-02-18 14:34:09 +0530

    Rewards Calculation System Commit 2 , Added comments

--- springboot-app/src/main/java/com/rewardcalcuationsystem/springboot_app/rewardsCalculator/RewardService.java
+++ springboot-app/src/main/java/com/rewardcalcuationsystem/springboot_app/rewardsCalculator/RewardService.java
@@ -6,8 +6,11 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * RewardService is a service class that contains the business logic for calculating reward points.
@@ -18,6 +21,9 @@ public class RewardService {
     @Autowired
     private TransactionRepository transactionRepository;
 
+    Logger logger
+            = Logger.getLogger(
+            RewardService.class.getName());
     /**
      * Calculates the reward points for a given transaction amount.
      *
@@ -43,17 +49,22 @@ public class RewardService {
      * @return a map of month names to reward points
      */
     public Map<String, Integer> getMonthlyRewards(Long customerId) {
-        Map<String, Integer> monthlyRewards = new HashMap<>();
+        Map<String, Integer> monthlyRewards = new LinkedHashMap<>();
         LocalDate now = LocalDate.now();
+        logger.log(Level.INFO, "now {0}", now);
 
         for (Month month : Month.values()) {
+            if (month.getValue() > now.getMonthValue()) {
+                break;
+            }
+            logger.log(Level.INFO, "month {0}", month);
             LocalDate startDate = LocalDate.of(now.getYear(), month, 1);
             LocalDate endDate = startDate.withDayOfMonth(startDate.lengthOfMonth());
-            List<Transaction> transactions = transactionRepository.findByCustomerIdAndTransactionDateBetween(customerId, startDate, endDate);
+            logger.log(Level.INFO, "startDate {0}", startDate);
+            logger.log(Level.INFO, "endDate {0}", endDate);
 
-            if (transactions.isEmpty()) {
-                throw new CustomerNotFoundException("No transactions found for customer ID: " + customerId);
-            }
+            List<Transaction> transactions = transactionRepository.findByCustomerIdAndTransactionDateBetween(customerId, startDate, endDate);
+            logger.log(Level.INFO, "transactions {0}", transactions);
 
             int monthlyPoints = transactions.stream()
                     .mapToInt(transaction -> calculatePoints(transaction.getAmount()))
@@ -63,7 +74,7 @@ public class RewardService {
         }
 
         return monthlyRewards;
-    }
+        }
 
     /**
      * Retrieves the total reward points for a specific customer.
